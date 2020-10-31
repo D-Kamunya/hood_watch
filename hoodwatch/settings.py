fee069b0c06d0e2679c2a42b1fe6a5df7aa77c7f
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-31 12:54:32 +0300

    Create and register django users app

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hood.apps.HoodConfig'
+    'hood.apps.HoodConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
