4e52bf6fc75629678ca07b618a76aeae00c03e07
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-31 13:33:11 +0300

    Create users register user view function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignUpForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
+def register_user(request):
+  if request.method == "POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        messages.success(request, f'Account for username {username} successfully created.')
+        return redirect('home_page')
+  else:
+      form = SignUpForm() 
+  return render(request, 'registration/registration_form.html', {'form': form})
