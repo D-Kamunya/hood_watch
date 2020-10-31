cfed2ea56295c3343697ec0077d43bbb6846e86b
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-31 14:55:38 +0300

    Create users profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+import datetime as dt
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = ImageField(blank=True, manual_crop="")
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.CharField(max_length=60,blank=True)
+    create_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
