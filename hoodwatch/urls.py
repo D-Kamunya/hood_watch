65e415617625c701b092910b8476839778a79225
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-31 14:33:26 +0300

    Create user logout url conf

--- hoodwatch/urls.py
+++ hoodwatch/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('',include('hood.urls')),
     path('accounts/register',user_views.register_user,name='register_user'),
     path('accounts/login',user_views.user_login,name='user_login'),
+    path('logout', user_views.user_logout, name='user_logout'),
 ]
