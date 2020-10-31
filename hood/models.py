5e2a754e3e899cb9df454facd471714481c71542
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-31 20:15:54 +0300

    Create hood neighbourhood model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+class NeighbourHood(models.Model):
+    hood_admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    hood_name = models.TextField(max_length=500)
+    hood_location = models.CharField(max_length=60, blank=True)
+    hood_occupants = models.IntegerField(default=0)
+    create_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.hood_name
