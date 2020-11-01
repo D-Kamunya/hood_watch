b08bebe3388ebe4596f75701c6bc2f6743ef5ee6
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-01 04:35:02 +0300

    Create hood home join hood modal for user to select hood

--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .decorators import user_has_hood
-
+from .models import NeighbourHood
 def home_page(request):
 
-  return render(request,'home.html')
+
+  hoods=NeighbourHood.get_all_neighbourhoods()
+  context={
+    'hoods':hoods
+  }
+  return render(request,'home.html',context)
