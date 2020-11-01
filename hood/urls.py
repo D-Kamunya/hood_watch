517c49f4228a963d8b37b00e33d8c8d3089e0b00
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-01 10:00:04 +0300

    Create hood e services urlconf

--- hood/urls.py
+++ hood/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
   path('',views.home_page,name='home_page'),
-  path('join/hood/<hood_id>',views.join_hood,name='join_hood')
+  path('join/hood/<hood_id>',views.join_hood,name='join_hood'),
+  path('hood/emergency-services',views.e_services,name='e_services')
 ]
